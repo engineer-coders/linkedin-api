bc03b57ae7d4a8028afc84190bb2cae04aba979a
Engineer Coders <engineer.coders@gmail.com>
2014-07-22 13:09:30 +0800

    Added storyboards for iPhone and iPad.

--- LinkedIN-API/LIAAppDelegate.m
+++ LinkedIN-API/LIAAppDelegate.m
@@ -14,8 +14,8 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    // self.window.backgroundColor = [UIColor whiteColor];
-    // [self.window makeKeyAndVisible];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
